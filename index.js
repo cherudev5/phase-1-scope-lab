8e91023c65b6bbeab1f2134dbeb24a8d37dbfee8
David <cheruiyotscripts@gmail.com>
2023-12-20 10:51:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName='bob'
 function upperCaseCustomerName(){
-    return customerName.toUpperCase();
+    console.log(customerName.toUpperCase());
 }
 
 function setBestCustomer(){
